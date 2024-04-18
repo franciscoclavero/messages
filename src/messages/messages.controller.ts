67cf7a1c5cc415e2722f9b353cb1f104b101a45d
franciscoclavero <franciscogkoclavero@gmail.com>
2024-04-17 21:20:01 -0300

    feat: Refactor MessagesController to use MessagesService for message retrieval and creation

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,28 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  private messagesService: MessagesService;
+
+  constructor() {
+    // TODO: Change to use dependency injection
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 }
